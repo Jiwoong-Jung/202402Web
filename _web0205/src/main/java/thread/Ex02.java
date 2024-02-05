eeddd742a3d734e2e7ed04587cd278987a40b562
Jiwoong-Jung <jjw981119@naver.com>
2024-02-05 15:44:53 +0900

    2024-02-05 오후 스레드

--- _web0205/src/main/java/thread/Ex02.java
+++ _web0205/src/main/java/thread/Ex02.java
@@ -6,7 +6,20 @@ public class Ex02 {
 		Task task = new Task();
 		Thread th = new Thread(task);
 		th.start();
-
+		mainTask();
+	}
+	public static void mainTask() {
+		System.out.println("시작");
+		for (int i=0; i < 5; i++) {
+			System.out.println("진행");
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.out.println("끝");
 	}
 
 }
