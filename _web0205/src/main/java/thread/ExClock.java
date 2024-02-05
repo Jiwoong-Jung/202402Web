970038006917f068e1a2d3a099f6585633a908d8
Jiwoong-Jung <jjw981119@naver.com>
2024-02-05 16:23:06 +0900

    2024-02-05 오후 스레드

--- _web0205/src/main/java/thread/ExClock.java
+++ _web0205/src/main/java/thread/ExClock.java
@@ -7,12 +7,21 @@ public class ExClock {
 
 	public static void main(String[] args) {
 		System.out.println(LocalDateTime.now());
-		LocalDateTime localDateTime = LocalDateTime.now();
-		String localDateTimeFormat1 
-		= localDateTime.format(
-				DateTimeFormatter
-				.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초"));
-		System.out.println(localDateTimeFormat1);
+		
+		for (int i=0; i < 5; i++) {
+			LocalDateTime localDateTime = LocalDateTime.now();
+			String localDateTimeFormat1 
+			= localDateTime.format(
+					DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초"));
+			System.out.println(localDateTimeFormat1);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.out.println("time out");
 
 	}
 
