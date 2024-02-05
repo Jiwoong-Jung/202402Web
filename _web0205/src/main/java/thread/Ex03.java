babd1f1b5c77f6fde9ef8a7093a4bfe4c7d7255b
Jiwoong-Jung <jjw981119@naver.com>
2024-02-05 15:46:27 +0900

    2024-02-05 오후 스레드

--- _web0205/src/main/java/thread/Ex03.java
+++ _web0205/src/main/java/thread/Ex03.java
@@ -22,7 +22,20 @@ public class Ex03 {
 			}
 		});
 		th.start();
-
+		mainTask();
+	}
+	public static void mainTask() {
+		System.out.println("시작");
+		for (int i=0; i < 5; i++) {
+			System.out.println("진행");
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.out.println("끝");
 	}
 
 }
