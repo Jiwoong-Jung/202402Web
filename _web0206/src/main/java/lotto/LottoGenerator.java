c666dc27788bb1d1b1d3949efe0fd1041063ebc2
Jiwoong-Jung <jjw981119@naver.com>
2024-02-06 16:02:23 +0900

    2024-02-06 오후 컬렉션 HashSet

--- _web0206/src/main/java/lotto/LottoGenerator.java
+++ _web0206/src/main/java/lotto/LottoGenerator.java
@@ -1,6 +1,10 @@
 package lotto;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
 public class LottoGenerator {
     public static void main(String[] args) {
