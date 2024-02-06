6556120236ea3f5f8403d91b39c53c675955e923
Jiwoong-Jung <jjw981119@naver.com>
2024-02-06 16:17:52 +0900

    2024-02-06 오후 컬렉션 HashSet

--- _web0206/src/main/java/sec01/exam05/HashSetExample.java
+++ _web0206/src/main/java/sec01/exam05/HashSetExample.java
@@ -6,8 +6,12 @@ public class HashSetExample {
 	public static void main(String[] args) {
 		Set<Member> set = new HashSet<Member>();
 		
-		set.add(new Member("홍길동", 30));
-		set.add(new Member("홍길동", 30));
+		Member mb1 = new Member("홍길동", 30);
+		Member mb2 = new Member("홍길동", 30);
+		System.out.println(mb1.hashCode());
+		System.out.println(mb2.hashCode());
+		set.add(mb1);
+		set.add(mb2);
 		
 		System.out.println("총 객체수 : " + set.size());
 	}
